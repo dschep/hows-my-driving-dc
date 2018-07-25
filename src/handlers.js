48966d469b51cba7da2020c62c8600387677cd2a
Daniel Schep <dschep@gmail.com>
2018-07-24 23:07:07 -0400

    subscribe lambda and the actual webhookgit difftool -t meld -d git difftool -t meld -d  IT'S ALIVE

--- src/handlers.js
+++ src/handlers.js
@@ -1,5 +1,7 @@
+const {readFileSync} = require('fs');
+
 const middy = require('middy');
-const {ssm} = require('middy/middlewares');
+const {ssm, jsonBodyParser} = require('middy/middlewares');
 const Twitter = require('twitter');
 
 const setup = require('./puppeteer/setup');
@@ -20,7 +22,6 @@ module.exports.test = middy(async (event, context) => {
   return lookupPlate(browser, client, event.state, event.number);
 });
 module.exports.test.use(ssm({
-  cache: true,
   names: {
     CONSUMER_KEY: '/howsmydriving/consumer_key',
     CONSUMER_SECRET: '/howsmydriving/consumer_secret',
@@ -69,6 +70,98 @@ module.exports.register = middy(async (event) => {
   ));
 });
 module.exports.register.use(ssm({
+  names: {
+    CONSUMER_KEY: '/howsmydriving/consumer_key',
+    CONSUMER_SECRET: '/howsmydriving/consumer_secret',
+    ACCESS_TOKEN: '/howsmydriving/access_token',
+    ACCESS_TOKEN_SECRET: '/howsmydriving/access_token_secret',
+  },
+}));
+
+module.exports.subscribe = middy(async (/*event*/) => {
+  const client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  });
+  // eslint-disable-next-line no-undef
+  return new Promise((resolve, reject) => client.post(
+    `/account_activity/all/dev/subscriptions.json`,
+    (error, data) => {
+      if (error) {
+        reject(JSON.stringify(error));
+      } else {
+        resolve(data);
+      }
+    }
+  ));
+});
+module.exports.subscribe.use(ssm({
+  names: {
+    CONSUMER_KEY: '/howsmydriving/consumer_key',
+    CONSUMER_SECRET: '/howsmydriving/consumer_secret',
+    ACCESS_TOKEN: '/howsmydriving/access_token',
+    ACCESS_TOKEN_SECRET: '/howsmydriving/access_token_secret',
+  },
+}));
+
+module.exports.webhook = middy(async (event, context) => {
+  console.log(event);
+  context.callbackWaitsForEmptyEventLoop = false;
+  const browser = await setup.getBrowser();
+  const client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  });
+  if (!event.body.tweet_create_events) {
+    return;
+  }
+  const [, state, number] = event.body.tweet_create_events[0].text.match(/\b([a-zA-Z]{2}):([a-zA-Z0-9]+)\b/);
+  const ticketsOrError = await lookupPlate(browser, state.toUpperCase(), number);
+  console.log('lets tweet!');
+  const status = {
+    in_reply_to_status_id: event.body.tweet_create_events[0].id_str,
+    status: `@${event.body.tweet_create_events[0].user.screen_name} `,
+  };
+  if (ticketsOrError.startsWith('/')) {
+    const data = readFileSync('/tmp/tickets.png');
+    console.log('loaded image');
+    status.status += `${state} ${number} has outstanding tickets:`;
+    // eslint-disable-next-line no-undef
+    status.media_ids = await new Promise((resolve, reject) => client.post(
+      'media/upload',
+      {media: data},
+      (error, media) => {
+        if (!error) {
+          // If successful, a media object will be returned.
+          console.log(media);
+          resolve(media.media_id_string);
+        } else {
+          console.log('problem uploading', error);
+          reject(error);
+        }
+      }));
+  } else {
+    status.status += ticketsOrError;
+  }
+  // eslint-disable-next-line no-undef
+  return new Promise((resolve, reject) => client.post(
+    'statuses/update',
+    status,
+    (error, tweet) => {
+      if (!error) {
+        console.log(tweet);
+        resolve();
+      } else {
+        console.log('problem tweeting', error);
+        reject(error);
+      }
+    }));
+});
+module.exports.webhook.use(ssm({
   cache: true,
   names: {
     CONSUMER_KEY: '/howsmydriving/consumer_key',
@@ -77,3 +170,4 @@ module.exports.register.use(ssm({
     ACCESS_TOKEN_SECRET: '/howsmydriving/access_token_secret',
   },
 }));
+module.exports.webhook.use(jsonBodyParser())
--- src/lookupPlate.js
+++ src/lookupPlate.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-undef */
-const {readFileSync} = require('fs');
 const Tesseract = require('tesseract.js');
 
 const screenshotDOMElement = require('./screenshotDOMElement.js');
 
-module.exports = async (browser, client, state = 'DC', number = 'ey9285') => {
+module.exports = async (browser, state = 'DC', number = 'ey9285') => {
   const page = await browser.newPage();
   await page.setViewport({height: 768, width: 1024});
   await page.goto('https://prodpci.etimspayments.com/pbw/include/dc_parking/input.jsp',
@@ -60,41 +59,6 @@ module.exports = async (browser, client, state = 'DC', number = 'ey9285') => {
   });
   console.log('screenshoted tickets!');
 
-  if (client) {
-    console.log('lets tweet!');
-    const data = readFileSync('/tmp/tickets.png');
-    console.log('loaded image');
-    return new Promise((resolve, reject) => client.post(
-      'media/upload',
-      {media: data},
-      function(error, media) {
-        if (!error) {
-          // If successful, a media object will be returned.
-          console.log(media);
-
-          // Lets tweet it
-          const status = {
-            status: `${state} ${number} has outstanding tickets:`,
-            media_ids: media.media_id_string,
-          };
-
-          client.post('statuses/update', status,
-                      function(error, tweet) {
-                        if (!error) {
-                          console.log(tweet);
-                          resolve();
-                        } else {
-                          console.log('problem tweeting', error);
-                          reject(error);
-                        }
-                      });
-        } else {
-          console.log('problem uploading', error);
-          reject(error);
-        }
-      }));
-  }
-
-  return captcha;
+  return '/tmp/tickets.png';
 };
 /* eslint-disable no-enable */
