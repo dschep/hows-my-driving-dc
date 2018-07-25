af21e4fec9a6d275a9658f61da61750b4e06ec60
Daniel Schep <dschep@gmail.com>
2018-07-24 21:29:37 -0400

    Don't log twitter secrets!

--- src/handlers.js
+++ src/handlers.js
@@ -10,12 +10,6 @@ module.exports.test = middy(async (event, context) => {
   // For keeping the browser launch
   context.callbackWaitsForEmptyEventLoop = false;
   const browser = await setup.getBrowser();
-  console.log({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  });
   const client = new Twitter({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
