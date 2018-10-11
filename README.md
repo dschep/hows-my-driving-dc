632d1428e31b8f12ba30a6260b5d1d804c981fba
Daniel Schep <dschep@gmail.com>
2018-10-11 17:53:58 -0400

    the ghetto archiver! (#12)

--- README.md
+++ README.md
@@ -15,6 +15,15 @@ Bootstraped from https://github.com/sambaiz/puppeteer-lambda-starter-kit
 ## Press
  * [2018-09-10 - Washingtonian](https://www.washingtonian.com/2018/09/10/twitter-bot-finds-worst-dc-drivers-parking/)
  * [2018-09-20 - Washignton Post](https://www.washingtonpost.com/local/trafficandcommuting/hows-dc-driving-this-new-tool-instantly-reveals-any-drivers-traffic-violations/2018/09/19/860d8fea-bb8d-11e8-9812-a389be6690af_story.html)
+
+## Archiver
+There's a script for archiving the bot's history to a sqlite db. run it like this:
+```
+sls invoke local  -f archive
+```
+I've even checked in the sqlite db for other to peruse:
+https://github.com/dschep/hows-my-driving-dc/blob/master/archive.db
+
 ## Screenshot
 ![](./screenshot.png)
 
--- src/handlers.js
+++ src/handlers.js
@@ -9,6 +9,8 @@ const lookupPlate = require('./lookupPlate.js');
 const crc = require('./crc.js');
 const getHighscore = require('./getHighscore.js');
 
+const PLATE_REGEX = /\b([a-zA-Z]{2}):([a-zA-Z0-9]+)\b/;
+
 module.exports.test = middy(async (event, context) => {
   // For keeping the browser launch
   context.callbackWaitsForEmptyEventLoop = false;
@@ -133,7 +135,7 @@ module.exports.webhook = middy(async (event, context) => {
     const text = event.body.tweet_create_events[0].truncated
       ? event.body.tweet_create_events[0].extended_tweet.full_text
       : event.body.tweet_create_events[0].text;
-    [, state, number] = text.match(/\b([a-zA-Z]{2}):([a-zA-Z0-9]+)\b/);
+    [, state, number] = text.match(PLATE_REGEX);
   } catch (e) {
     console.log(e);
     return;
@@ -206,3 +208,124 @@ module.exports.webhook.use(
   })
 );
 module.exports.webhook.use(jsonBodyParser());
+
+module.exports.archive = middy(async () => {
+  const sqlite = require('sqlite');
+  const client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  });
+  const db = await sqlite.open('archive.db');
+
+  await db.run(
+    `
+    CREATE TABLE IF NOT EXISTS tweets (
+      tweet_id text,
+      created_at timestamp,
+      content text,
+      state varchar(2),
+      number text,
+      amount double,
+      user text,
+      summoning_text text,
+      zero_reason text
+    )
+    `
+  );
+
+  let { since_id } = await db.get('SELECT max(tweet_id) since_id FROM tweets');
+  let max_id;
+  while (true) { // eslint-disable-line no-constant-condition
+    const ownTweets = await client.get('/statuses/user_timeline.json', {
+      screen_name: 'howsmydrivingdc',
+      count: 200,
+      since_id: since_id || undefined, // eslint-disable-line no-undef
+      max_id
+    });
+    for (const {
+      text,
+      id_str,
+      created_at,
+      in_reply_to_status_id_str
+    } of ownTweets) {
+      if (id_str === max_id) {
+        continue;
+      }
+      max_id = id_str;
+      const match = text.match(
+        /@\S+ ([a-zA-Z]{2}):([a-zA-Z0-9]+) has \$(\d+(\.\d+)?) in outstanding tickets:/
+      );
+      let summoningTweet;
+      try {
+        summoningTweet = await client.get('/statuses/show.json', {
+          id: in_reply_to_status_id_str,
+          tweet_mode: 'extended'
+        });
+      } catch (e) {
+        console.log('Summoning tweet deleted');
+      }
+      if (match) {
+        await db.run(
+          `INSERT INTO tweets (tweet_id, created_at, content, state, number, amount, user, summoning_text)
+           VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+          [
+            id_str,
+            new Date(created_at).toISOString(),
+            text,
+            match[1],
+            match[2],
+            match[3],
+            summoningTweet && summoningTweet.user.screen_name,
+            summoningTweet && summoningTweet.full_text
+          ]
+        );
+        console.log(`ADDED ${match[1]}:${match[2]} ${match[3]}`);
+      } else {
+        let zeroReason;
+        if (text.includes('balance of $0')) {
+          zeroReason = 'paid';
+        } else if (text.includes('not found')) {
+          zeroReason = 'unfound';
+        } else {
+          console.log(`SKIPPED ${text} - not a response to summoning`);
+          continue;
+        }
+        const [, state, number] = summoningTweet
+          ? summoningTweet.full_text.match(PLATE_REGEX)
+          : [null, null, null];
+        await db.run(
+          `INSERT INTO tweets (tweet_id, created_at, content, state, number, amount, user, summoning_text, zero_reason)
+           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+          [
+            id_str,
+            new Date(created_at).toISOString(),
+            text,
+            state,
+            number,
+            0,
+            summoningTweet && summoningTweet.user.screen_name,
+            summoningTweet && summoningTweet.full_text,
+            zeroReason
+          ]
+        );
+        console.log(`ADDED ${state}:${number} 0`);
+      }
+    }
+    if (ownTweets.length <= 1)
+      // bc of how max_id works, that tweet itself is always returned
+      return;
+  }
+});
+module.exports.archive.use(
+  ssm({
+    cache: true,
+    names: {
+      CONSUMER_KEY: '/howsmydriving/consumer_key',
+      CONSUMER_SECRET: '/howsmydriving/consumer_secret',
+      ACCESS_TOKEN: '/howsmydriving/access_token',
+      ACCESS_TOKEN_SECRET: '/howsmydriving/access_token_secret'
+    }
+  })
+);
